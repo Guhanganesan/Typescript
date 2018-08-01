84aa825a5512a074cf55773257f76df938313b59
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-08-01 19:49:11 +0530

    Create Tuples.ts

---
+++ Tuples.ts
@@ -0,0 +1,11 @@
+// TUPLES
+var x = [1, "guhan", 4.5, "guhan@gmail.com"]
+console.log(x[2]);
+x[4] = "anbu";
+console.log(x);
+x.push("Kannan");
+console.log(x);
+x.pop();
+console.log(x);
+x[0] = "Kumar";
+console.log(x);
