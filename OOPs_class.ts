15b8930c607f1d94af64645579b415da41ff70c3
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-08-03 08:29:22 +0530

    Create OOPs_class.ts

---
+++ OOPs_class.ts
@@ -0,0 +1,26 @@
+class Kala
+{
+    public name = "Kala";
+     ondrser()
+    {
+         console.log("Ondruser");
+    }
+   constructor() {
+        console.log("Karpi");
+    }
+    puratchi()
+    {
+        console.log("Puratchi");
+    }
+
+}
+
+var rajni = new Kala();
+var ranjith = new Kala();
+ranjith.ondrser();
+rajni.puratchi();
+
+Result:
+Karpi
+VM266:7 Ondruser
+VM266:10 Puratchi
