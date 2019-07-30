5c3c01da504763de5d1be69174c464534271863e
Guhan Ganesan <bharadhiguhan@ymail.com>
2019-07-29 19:48:02 -0700

    Update Class.ts

--- Class.ts
+++ Class.ts
@@ -1,3 +1,17 @@
+/*
+Class − A class can be defined as a template/blueprint that describes 
+the behavior/state that the object of its type support.
+ 
+It contains real values instead of variables.
+
+Object − Objects have states and behaviors.
+An object is a specific instance of a class;
+ 
+Example: A dog has states - color, name, breed as well as 
+behaviors – wagging the tail, barking, eating. 
+
+*/
+
 class College
 {
     name="Agni";
